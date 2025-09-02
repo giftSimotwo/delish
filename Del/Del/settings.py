b2a6addf4e035deb66805b2d22802e44a1f08c22
Gift Kiplagat <giftsimotwo@gmail.com>
2025-09-02 08:31:27 +0300

    installed gunicorn

--- Del/Del/settings.py
+++ Del/Del/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-key")
 
 # 'django-insecure-2tfsgvgq(failr!ltsgq^@%71u2f8r2qbo)-e&fk^)r)d^g3@@'
 
